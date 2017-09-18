fe15cd52483f5686d06a5f176e8a73ecc7d659c3
mjjackman <mjjackman2000@yahoo.co.uk>
2017-09-18 17:03:13 +0100

    moved from bitbucket to github

--- kindergarten/app/controllers/groups.rb
+++ kindergarten/app/controllers/groups.rb
@@ -1,26 +1,9 @@
 Kindergarten::App.controllers :groups do
+  layout :layout
   
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-    get :index do
-
+  get :index do
+    @group = Group.find(params[:id])
+    render :index
   end
 
   get :new do
